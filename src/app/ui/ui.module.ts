f93c1c4f85d6352182f5bf96c79ad735ae007201
Bartłomiej Piepióra <bartlomiej.piepiora@bakk.com>
2019-01-14 11:50:53 +0100

    add graph menu

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,14 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LayoutComponent } from './layout/layout.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LayoutComponent } from "./layout/layout.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { GraphMenu } from "./graph-menu/graph-menu.component";
 
 @NgModule({
-  declarations: [LayoutComponent, HeaderComponent, FooterComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [LayoutComponent]
+  declarations: [LayoutComponent, HeaderComponent, FooterComponent, GraphMenu],
+  imports: [CommonModule],
+  exports: [LayoutComponent, GraphMenu]
 })
-export class UiModule { }
+export class UiModule {}
